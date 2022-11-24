63c7fd6e087aa92e5db6934dfc2df97eec29ac86
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2022-11-24 10:36:35 +0100

    First unit test implemented

--- SolutionDownloaderCore/src/Pg.SolutionDownloaderCore/Pg.SolutionDownloaderCore.Tests/Data/InputArgumentReaderTest.cs
+++ SolutionDownloaderCore/src/Pg.SolutionDownloaderCore/Pg.SolutionDownloaderCore.Tests/Data/InputArgumentReaderTest.cs
@@ -1,11 +1,55 @@
-﻿namespace Pg.SolutionDownloaderCore.Tests.Data
+﻿using Microsoft.Crm.Sdk.Messages;
+using Pg.SolutionDownloaderCore.Data;
+using Pg.SolutionDownloaderCore.Model;
+using static System.Net.WebRequestMethods;
+
+namespace Pg.SolutionDownloaderCore.Tests.Data
 {
     public class InputArgumentReaderTest
     {
         [Fact]
         public void GetInput_ValidArgs_ReturnDto()
         {
-            throw new NotImplementedException();    
+            var expected = new InputDto()
+            {
+                DataverseUrl = "http:myinstance.crm4.dynamics.com",
+                ApplicationId = "1234",
+                ClientSecret = "09876543321",
+                SolutionName = "MySolution",
+                IsManaged = true,
+                OutputDir = "."
+            }; 
+
+            string[] args = new string[6]; 
+            args[0] = $"-url:http:{expected.DataverseUrl}";
+            args[1] = $"-appId:{expected.ApplicationId}";
+            args[2] = $"-clientSecret:{expected.ClientSecret}";
+            args[3] = $"-solution:{expected.SolutionName}";
+            args[4] = $"-isManaged:{expected.IsManaged}";
+            args[5] = $"-outputDir:{expected.OutputDir}";
+
+            var reader = new InputArgumentReader();
+            var actual = reader.GetInput(args);
+
+            Assert.Multiple(
+                () => Assert.Equal(expected.DataverseUrl, actual.DataverseUrl),
+                () => Assert.Equal(expected.ApplicationId, actual.ApplicationId),
+                () => Assert.Equal(expected.ClientSecret, actual.ClientSecret),
+                () => Assert.Equal(expected.SolutionName, actual.SolutionName),
+                () => Assert.Equal(expected.IsManaged, actual.IsManaged),
+                () => Assert.Equal(expected.OutputDir, actual.OutputDir)
+            );
+        }
+
+        public void GetInput_ValidArgsWithoutDefaults_ReturnDto()
+        {
+            string[] args = new string[5];
+            args[0] = "-url:http:myinstance.crm4.dynamics.com";
+            args[1] = "-appId:";
+            args[2] = "-clientSecret:";
+            args[3] = "-solution:";
+
+            throw new NotImplementedException();
         }
 
         [Fact]
@@ -39,13 +83,13 @@
         }
 
         [Fact]
-        public void GetInput_MisingIsManaged_ThrowException()
+        public void GetInput_MisingIsManaged_UseDefaultValue()
         {
             throw new NotImplementedException();
         }
 
         [Fact]
-        public void GetInput_MisingOutputDir_ThrowException()
+        public void GetInput_MisingOutputDir_UseDefaultValue()
         {
             throw new NotImplementedException();
         }
