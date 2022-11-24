3b70d99ed322cc947abec335fa5cf9c4e4d41668
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2022-11-24 10:01:33 +0100

    Test project added to the solution

---
+++ SolutionDownloaderCore/src/Pg.SolutionDownloaderCore/Pg.SolutionDownloaderCore.Tests/Data/InputArgumentReaderTest.cs
@@ -0,0 +1,54 @@
+﻿namespace Pg.SolutionDownloaderCore.Tests.Data
+{
+    public class InputArgumentReaderTest
+    {
+        [Fact]
+        public void GetInput_ValidArgs_ReturnDto()
+        {
+            throw new NotImplementedException();    
+        }
+
+        [Fact]
+        public void GetInput_InvalidArgsCount_ThrowException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetInput_MisingDataverseUrl_ThrowException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetInput_MisingApplicationId_ThrowException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetInput_ClientSecret_ThrowException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetInput_MisingSolutionName_ThrowException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetInput_MisingIsManaged_ThrowException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetInput_MisingOutputDir_ThrowException()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
