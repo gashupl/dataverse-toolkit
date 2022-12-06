7c44cbdc3a844b3f31aedee245c823a8483598ff
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2022-12-06 08:36:19 +0100

    Empty unit tests added

---
+++ SolutionDownloaderCore/src/Pg.SolutionDownloaderCore/Pg.SolutionDownloaderCore.Tests/Services/SolutionServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg.SolutionDownloaderCore.Tests.Services
+{
+	public class SolutionServiceTest
+	{
+		[Fact]
+		public void DownloadSolution_ValidResponse()
+		{
+			throw new NotImplementedException(); 
+		}
+
+		[Fact]
+		public void DownloadSolution_MissingResponse()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
