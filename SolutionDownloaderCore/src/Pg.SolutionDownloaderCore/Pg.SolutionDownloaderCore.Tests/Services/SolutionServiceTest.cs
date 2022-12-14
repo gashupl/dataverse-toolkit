b5f907211106390dcd269547ddda6a0749bca619
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2022-12-14 09:08:46 +0100

    Additional unit tests implemented

--- SolutionDownloaderCore/src/Pg.SolutionDownloaderCore/Pg.SolutionDownloaderCore.Tests/Services/SolutionServiceTest.cs
+++ SolutionDownloaderCore/src/Pg.SolutionDownloaderCore/Pg.SolutionDownloaderCore.Tests/Services/SolutionServiceTest.cs
@@ -1,23 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using Pg.SolutionDownloaderCore.Data;
+using Pg.SolutionDownloaderCore.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Pg.SolutionDownloaderCore.Tests.Services
 {
 	public class SolutionServiceTest
 	{
+        private readonly ILoggerFactory _loggerFactory;
+
+		public SolutionServiceTest()
+		{
+			var serviceProvider = new ServiceCollection()
+				.AddLogging(opt => opt.AddConsole().SetMinimumLevel(LogLevel.Trace))
+                .BuildServiceProvider();
+            _loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+        }
+
 		[Fact]
 		public void DownloadSolution_ValidResponse()
 		{
-			throw new NotImplementedException(); 
-		}
+			bool fileSaved = false; 
+			var repo = new Mock<ISolutionRepository>();
+			repo.Setup(r => r.Get(It.IsAny<string>(), It.IsAny<bool>()))
+				.Returns(new ExportSolutionResponse());
+			
+			var file  = new Mock<IFile>();
+			file.Setup(f => f.WriteAllBytes(It.IsAny<string>(), It.IsAny<byte[]>()))
+				.Callback(() =>
+					{
+						fileSaved = true; 
+					});
+
+			var service = new SolutionService(repo.Object, file.Object, _loggerFactory);
+			service.DownloadSolution(String.Empty, String.Empty, false);
+
+			Assert.True(fileSaved); 
+        }
 
 		[Fact]
 		public void DownloadSolution_MissingResponse()
 		{
-			throw new NotImplementedException();
-		}
-	}
+            bool fileSaved = false;
+            var repo = new Mock<ISolutionRepository>();
+            repo.Setup(r => r.Get(It.IsAny<string>(), It.IsAny<bool>()))
+                .Returns<ISolutionRepository>(null);
+
+            var file = new Mock<IFile>();
+            file.Setup(f => f.WriteAllBytes(It.IsAny<string>(), It.IsAny<byte[]>()))
+                .Callback(() =>
+                {
+                    fileSaved = true;
+                });
+
+            var service = new SolutionService(repo.Object, file.Object, _loggerFactory);
+            service.DownloadSolution(String.Empty, String.Empty, false);
+
+            Assert.False(fileSaved);
+        }
+
+        [Fact]
+        public void DownloadSolution_HandleDataverseCallException() 
+		{
+            bool fileSaved = false;
+            var repo = new Mock<ISolutionRepository>();
+            repo.Setup(r => r.Get(It.IsAny<string>(), It.IsAny<bool>()))
+                .Throws(new DataverseCallException(String.Empty, new Exception()));
+
+            var file = new Mock<IFile>();
+            file.Setup(f => f.WriteAllBytes(It.IsAny<string>(), It.IsAny<byte[]>()))
+                .Callback(() =>
+                {
+                    fileSaved = true;
+                });
+
+            var service = new SolutionService(repo.Object, file.Object, _loggerFactory);
+            service.DownloadSolution(String.Empty, String.Empty, false);
+
+            Assert.False(fileSaved);
+        }
+
+        [Fact]
+        public void DownloadSolution_HandleException()
+        {
+            bool fileSaved = false;
+            var repo = new Mock<ISolutionRepository>();
+            repo.Setup(r => r.Get(It.IsAny<string>(), It.IsAny<bool>()))
+                .Throws(new Exception());
+
+            var file = new Mock<IFile>();
+            file.Setup(f => f.WriteAllBytes(It.IsAny<string>(), It.IsAny<byte[]>()))
+                .Callback(() =>
+                {
+                    fileSaved = true;
+                });
+
+            var service = new SolutionService(repo.Object, file.Object, _loggerFactory);
+            service.DownloadSolution(String.Empty, String.Empty, false);
+
+            Assert.False(fileSaved);
+        }
+    }
 }
