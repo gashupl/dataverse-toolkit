b78689191505f2e9d91281c8fed03950e3c8739a
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2023-04-19 08:59:55 +0200

    Updated installation script for solution import

--- DataverseTags/scripts/install/install.ps1
+++ DataverseTags/scripts/install/install.ps1
@@ -12,13 +12,21 @@ write-host "Secret: $secret";
 write-host "TenantId: $tenantId";
 
 write-host "Generating temporary authentication profile name...";
-$name = New-GUID;
+$controlsSolutionPath = ".\solutions\DataverseTagsControls.zip";
+$customizationSolutionPath = ".\solutions\DataverseTags_managed.zip"; 
+$name = (-join ((65..90) + (97..122) | Get-Random -Count 20 | % {[char]$_})) #Generate random 20-characters string
 write-host "Temporary authentication profile name: $name";
 write-host "Creating authentication profile: $name";
 pac auth create --url $url --name $name --applicationId $appId --clientSecret $secret --tenant $tenantId
+write-host "Authentication profile $name created ";
 pac auth select --name $name
-write-host "Authentication profile $name selected and updated";
-
+write-host "Authentication profile $name selected";
+write-host "Importing solution $controlsSolutionPath ...";
+pac solution import --path $controlsSolutionPath
+write-host "Solution $controlsSolutionPath imported";
+write-host "Importing solution $customizationSolutionPath  ...";
+pac solution import --path $customizationSolutionPath 
+write-host "Solution $customizationSolutionPath imported";
 
 write-host "Deleting authentication profile $name";
 pac auth delete --name $name
