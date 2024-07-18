c7057f61f6208be98cad1f999ead9b5a3706f97f
Piotr Gaszewski <me@piotrgaszewski.pl>
2024-07-18 15:31:04 +0200

    Initial commit of the plugin project

---
+++ DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.Plugin/TaskPlugin.cs
@@ -0,0 +1,46 @@
+using Confluent.Kafka;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Pg.Dataverse.Kafka.Plugin
+{
+    public class TaskPlugin : PluginBase
+    {
+        public TaskPlugin(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(TaskPlugin))
+        {
+            // TODO: Implement your custom configuration handling
+            // https://docs.microsoft.com/powerapps/developer/common-data-service/register-plug-in#set-configuration-data
+        }
+
+        // Entry point for custom business logic execution
+        protected override void ExecuteDataversePlugin(ILocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null)
+            {
+                throw new ArgumentNullException(nameof(localPluginContext));
+            }
+
+            var context = localPluginContext.PluginExecutionContext;
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                var entity = (Entity)context.InputParameters["Target"];
+
+                // Check for entity name on which this plugin would be registered
+                if (entity.LogicalName == nameof(TaskPlugin))
+                { 
+                    var config = new ProducerConfig
+                    {
+                        BootstrapServers = "host1:9092",
+                    };
+
+                    using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                    {
+                        //...
+                    }
+                }
+            }
+        }
+    }
+}
