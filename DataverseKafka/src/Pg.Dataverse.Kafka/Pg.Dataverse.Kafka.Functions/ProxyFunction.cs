8f6367223a373def05cfcbef1a9d50f5339456b4
Piotr Gaszewski <me@piotrgaszewski.pl>
2024-07-23 16:50:37 +0200

    Reading Dataverse entity and message names

--- DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.Functions/ProxyFunction.cs
+++ DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.Functions/ProxyFunction.cs
@@ -3,41 +3,43 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
 
 namespace Pg.Dataverse.Kafka.Functions
 {
-    public class Function1
+    public class ProxyFunction
     {
-        private readonly ILogger<Function1> _logger;
+        private readonly ILogger<ProxyFunction> _logger;
 
-        public Function1(ILogger<Function1> logger)
+        public ProxyFunction(ILogger<ProxyFunction> logger)
         {
             _logger = logger;
         }
 
-        [Function("Function1")]
+        [Function("ProxyFunction")]
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("Received a webhook call from Dataverse.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            JObject data = JObject.Parse(requestBody);
 
             // Assuming the payload contains an entity name and operation
-            string entityName = data?.entityName;
-            string operation = data?.operation;
+            JToken? entityName = data?["PrimaryEntityName"];
+            JToken? operation = data?["MessageName"];
 
-            _logger.LogInformation($"Entity: {entityName}, Operation: {operation}");
+            _logger.LogInformation($"Entity: {entityName?.ToString()}, Operation: {operation?.ToString()}");
 
             // Here you can add your logic to handle different entities and operations
             // For example, processing a create operation for a specific entity
-            if (entityName == "task" && operation == "create")
+            if (entityName.ToString() == "task" && operation.ToString() == "Create")
             {
                 // Process the create operation for the contact entity
                 _logger.LogInformation("Processing create operation for contact entity.");
             }
 
-            return new OkResult();
+            return new OkObjectResult("OK!");
         }
 
 
