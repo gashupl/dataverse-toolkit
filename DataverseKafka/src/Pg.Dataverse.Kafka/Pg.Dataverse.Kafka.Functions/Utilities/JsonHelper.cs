f9bc63c653abfd57f0ba60cda1ee29378a7839d4
Piotr Gaszewski <me@piotrgaszewski.pl>
2024-07-24 09:10:07 +0200

    Unecessary usings removed

--- DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.Functions/Utilities/JsonHelper.cs
+++ DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.Functions/Utilities/JsonHelper.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization.Json;
+﻿using System.Runtime.Serialization.Json;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Pg.Dataverse.Kafka.Functions.Utilities
 {
