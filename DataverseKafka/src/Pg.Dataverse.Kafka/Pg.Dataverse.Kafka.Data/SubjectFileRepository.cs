42b68c0fec9f4af5bdabb88184f35586a75449dc
Piotr Gaszewski <me@piotrgaszewski.pl>
2024-08-19 08:31:50 +0200

    - Added `System.Data.SqlClient` v4.8.6 to `Pg.Dataverse.Kafka.Data.csproj`. - Introduced `SubjectFileRepository` for appending text lines to files. - Introduced `SubjectSqlRepository` for inserting subject names into SQL DB.

---
+++ DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.Data/SubjectFileRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Pg.Dataverse.Kafka.Data
+{
+    public class SubjectFileRepository
+    {
+        private readonly string _filePath;
+
+        public SubjectFileRepository(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void AddLine(string text)
+        {
+            // Append the text to the file, followed by a newline character
+            File.AppendAllText(_filePath, text + Environment.NewLine);
+        }
+    }
+}
---
+++ DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.Data/SubjectSqlRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace Pg.Dataverse.Kafka.Data
+{
+    public class SubjectSqlRepository
+    {
+        private readonly string _connectionString;
+
+        public SubjectSqlRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public int InsertSubject(string subjectName)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command
+                    = new SqlCommand("INSERT INTO Subjects (SubjectText) OUTPUT INSERTED.Id VALUES (@SubjectText)", connection))
+                {
+                    command.Parameters.AddWithValue("@SubjectText", subjectName);
+
+                    // Execute the command and get the inserted ID
+                    int insertedId = (int)command.ExecuteScalar();
+                    return insertedId;
+
+                }
+            }
+        }
+    }
+}
