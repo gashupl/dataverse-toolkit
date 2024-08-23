9d6fcf23a247fe4abb5663306657c47a550ec124
Piotr Gaszewski <me@piotrgaszewski.pl>
2024-08-23 08:42:28 +0200

    A new project named `Pg.Dataverse.Kafka.FileWriter` has been added to the solution.

---
+++ DataverseKafka/src/Pg.Dataverse.Kafka/Pg.Dataverse.Kafka.FileWriter/Program.cs
@@ -0,0 +1,9 @@
+﻿using Pg.Dataverse.Kafka.Data;
+
+var fileRepo = new SubjectFileRepository(".\\kafka-event-log.txt"); 
+
+for(int i = 0; i < 10; i++)
+{
+    fileRepo.InsertSubject($"Hello from Kafka {i}");
+}
+
